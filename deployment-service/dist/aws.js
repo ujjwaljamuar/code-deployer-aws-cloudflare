4f0be03e0c61c0ef738fee5f4709d69d1130d828
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2024-03-05 18:17:44 +0530

    project completed

--- deployment-service/dist/aws.js
+++ deployment-service/dist/aws.js
@@ -8,9 +8,7 @@ const aws_sdk_1 = require("aws-sdk");
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
 const s3 = new aws_sdk_1.S3({
-    accessKeyId: "ba26fa4a0e8e28c1b5307c54d94ebae0",
-    secretAccessKey: "5e39d15245b05eb1148ddc1cfb22dcf57ce90a66db6acd9773dc319eb1c0fc74",
-    endpoint: "https://732f56d8ec1d69381f60cd9865ba8d62.r2.cloudflarestorage.com",
+    
 });
 // prefix - output/id
 async function downloadS3Folder(prefix) {
