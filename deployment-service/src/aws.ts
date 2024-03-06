30487434a186403752ef6ed5fdf53f57e6a9ce90
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2024-03-06 15:58:07 +0530

    added projec demo

--- deployment-service/src/aws.ts
+++ deployment-service/src/aws.ts
@@ -3,7 +3,11 @@ import path from "path";
 import fs from "fs";
 
 const s3 = new S3({
-    
+    accessKeyId: "ba26fa4a0e8e28c1b5307c54d94ebae0",
+    secretAccessKey:
+        "5e39d15245b05eb1148ddc1cfb22dcf57ce90a66db6acd9773dc319eb1c0fc74",
+    endpoint:
+        "https://732f56d8ec1d69381f60cd9865ba8d62.r2.cloudflarestorage.com",
 });
 
 // prefix - output/id
--- request-handler/dist/index.js
+++ request-handler/dist/index.js
@@ -6,7 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const aws_sdk_1 = require("aws-sdk");
 const express_1 = __importDefault(require("express"));
 const s3 = new aws_sdk_1.S3({
-    
+    accessKeyId: "ba26fa4a0e8e28c1b5307c54d94ebae0",
+    secretAccessKey: "5e39d15245b05eb1148ddc1cfb22dcf57ce90a66db6acd9773dc319eb1c0fc74",
+    endpoint: "https://732f56d8ec1d69381f60cd9865ba8d62.r2.cloudflarestorage.com",
 });
 const app = (0, express_1.default)();
 app.get("/*", async (req, res) => {
